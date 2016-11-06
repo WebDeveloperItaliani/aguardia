90877e3e65942992be0d243a1be37a1950057d5e
Francesco Malatesta <hellofrancesco@gmail.com>
2016-11-06 10:10:27 +0100

    Add HashtagValidator

---
+++ src/Validator/HashtagValidator.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AGuardia\Validator;
+
+
+class HashtagValidator
+{
+    public function validate($message)
+    {
+        if(preg_match('/(\[)?([ ]*)(#[a-zA-Z0-9 ]+)([ ]*)(\])?/', $message) === 0)
+            return false;
+
+        return true;
+    }
+}
---
+++ tests/Unit/Validator/HashtagValidatorTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AGuardia\Unit\Validator;
+
+use AGuardia\Validator\HashtagValidator;
+use PHPUnit\Framework\TestCase;
+
+class HashtagValidatorTest extends TestCase
+{
+    /* @var HashtagValidator */
+    private $validator;
+
+    public function setUp()
+    {
+        $this->validator = new HashtagValidator();
+
+        parent::setUp();
+    }
+
+    public function testValidMessage()
+    {
+        $this->assertTrue($this->validator->validate('[#hashtag #yo] this is a regular message.'));
+        $this->assertTrue($this->validator->validate('#hashtag #yo this is a regular message.'));
+    }
+
+    public function testNotValidMessage()
+    {
+        $this->assertFalse($this->validator->validate('this is not a regular message. I will be banned :('));
+    }
+}
