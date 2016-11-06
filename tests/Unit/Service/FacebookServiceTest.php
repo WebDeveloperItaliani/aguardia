b822fbfa1ec125ea445d065dcd51f9db0635b0cd
Francesco Malatesta <hellofrancesco@gmail.com>
2016-11-06 10:02:40 +0100

    Refactor and improve tests

--- tests/Unit/Service/FacebookServiceTest.php
+++ tests/Unit/Service/FacebookServiceTest.php
@@ -28,18 +28,14 @@ class FacebookServiceTest extends TestCase
 
     public function testGetLatestPosts()
     {
-        $this->client->expects($this->once())
-            ->method('request')
-            ->willReturn(new Response(200, [], file_get_contents('tests/Unit/Fixtures/get_latest_posts_ok.json')));
+        $this->prepareResponseWithFixture('get_latest_posts');
 
         $this->assertCount(2, $this->facebookService->getLatestPostsByFbGroupId('test_facebook_id'));
     }
 
     public function testGetLatestPostsWithNoResults()
     {
-        $this->client->expects($this->once())
-            ->method('request')
-            ->willReturn(new Response(200, [], file_get_contents('tests/Unit/Fixtures/get_latest_posts_empty.json')));
+        $this->prepareResponseWithFixture('get_latest_posts_empty');
 
         $this->assertCount(0, $this->facebookService->getLatestPostsByFbGroupId('test_facebook_id'));
     }
@@ -50,18 +46,14 @@ class FacebookServiceTest extends TestCase
      */
     public function testGetLatestPostsWithError()
     {
-        $this->client->expects($this->once())
-            ->method('request')
-            ->willReturn(new Response(200, [], file_get_contents('tests/Unit/Fixtures/get_latest_posts_error.json')));
+        $this->prepareResponseWithFixture('get_latest_posts_error');
 
         $this->facebookService->getLatestPostsByFbGroupId('test_facebook_id');
     }
 
     public function testGetPostById()
     {
-        $this->client->expects($this->once())
-            ->method('request')
-            ->willReturn(new Response(200, [], file_get_contents('tests/Unit/Fixtures/get_post_ok.json')));
+        $this->prepareResponseWithFixture('get_post');
 
         $this->assertEquals([
             "created_time" => "2016-11-05T19:51:24+0000",
@@ -76,18 +68,14 @@ class FacebookServiceTest extends TestCase
      */
     public function testGetPostByIdThrowsError()
     {
-        $this->client->expects($this->once())
-            ->method('request')
-            ->willReturn(new Response(200, [], file_get_contents('tests/Unit/Fixtures/get_post_error.json')));
+        $this->prepareResponseWithFixture('get_post_error');
 
         $this->facebookService->getPostById('nonexistent_post_id');
     }
 
     public function testDeletePostById()
     {
-        $this->client->expects($this->once())
-            ->method('request')
-            ->willReturn(new Response(200, [], file_get_contents('tests/Unit/Fixtures/delete_post.json')));
+        $this->prepareResponseWithFixture('delete_post');
 
         $this->facebookService->deletePostById('existent_post_id');
     }
@@ -98,18 +86,14 @@ class FacebookServiceTest extends TestCase
      */
     public function testDeletePostByIdThrowsError()
     {
-        $this->client->expects($this->once())
-            ->method('request')
-            ->willReturn(new Response(200, [], file_get_contents('tests/Unit/Fixtures/delete_post_error.json')));
+        $this->prepareResponseWithFixture('delete_post_error');
 
         $this->facebookService->deletePostById('nonexistent_post_id');
     }
 
     public function testCommentPost()
     {
-        $this->client->expects($this->once())
-            ->method('request')
-            ->willReturn(new Response(200, [], file_get_contents('tests/Unit/Fixtures/comment_post.json')));
+        $this->prepareResponseWithFixture('comment_post');
 
         $this->facebookService->commentPost('existent_post_id', 'Hello there!');
     }
@@ -120,10 +104,15 @@ class FacebookServiceTest extends TestCase
      */
     public function testCommentPostThrowsException()
     {
-        $this->client->expects($this->once())
-            ->method('request')
-            ->willReturn(new Response(200, [], file_get_contents('tests/Unit/Fixtures/comment_post_error.json')));
+        $this->prepareResponseWithFixture('comment_post_error');
 
         $this->facebookService->commentPost('nonexistent_post_id', 'World will never seen this :(');
+    }
+
+    private function prepareResponseWithFixture($fileName)
+    {
+        $this->client->expects($this->once())
+            ->method('request')
+            ->willReturn(new Response(200, [], file_get_contents('tests/Unit/Fixtures/' . $fileName . '.json')));
     }
 }
