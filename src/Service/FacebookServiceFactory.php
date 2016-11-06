5e008088fa22ddbc0443b2d9ae5794ca72f4741a
Francesco Malatesta <hellofrancesco@gmail.com>
2016-11-06 10:17:14 +0100

    Make the make function static

--- src/Service/FacebookServiceFactory.php
+++ src/Service/FacebookServiceFactory.php
@@ -10,7 +10,7 @@ class FacebookServiceFactory
     const BASE_URI = 'https://graph.facebook.com/';
     const FB_API_VERSION = 'v2.7';
 
-    public function make($accessToken)
+    public static function make($accessToken)
     {
         return new FacebookService(new Client([
             'base_uri' => self::BASE_URI . self::FB_API_VERSION
